9485839921a102ec47fc55bfc6dbd107ea63ee0f
soujanyac <soujanya@yellowmessenger.com>
2023-05-10 13:18:24 +0530

    basic css for todoist clone

--- W3A/src/App.js
+++ W3A/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="topbar">topbar</div>
+        <div className="content-container">
+            <div className="sidebar">sidebar</div>
+            <div className="content">
+                <div className="day-list">
+                    <h4>Saturday</h4>
+                    <ul>
+                        <li>one</li>
+                        <li>one</li>
+                        <li>one</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
